de17427ac4a77be3f56aabdea5191200bf18a1c6
anselme.rabouan <anselme.rabouan@lifecycle-software.com>
2024-05-22 18:47:52 +0200

    wip

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,8 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
+import url from 'postcss-url';
+import postcssImport from 'postcss-import';
 
 export default {
     input: 'src/components/app-drawer.ts',
@@ -13,15 +15,28 @@ export default {
         sourcemap: true
     },
     plugins: [
+        postcss({
+            inject: true,
+            extract: false,
+            plugins: [
+                postcssImport(),
+                url({
+                    url: 'inline',
+                    limit: 0, // Inline all files, regardless of size
+                    emitFiles: true, // Keep files in the output folder
+                    fileName: '[name][extname]', // Preserve the original name and extension
+                })
+            ],
+            minimize: true,
+            modules: {
+                generateScopedName: '[name]__[local]___[hash:base64:5]'
+            }
+        },
+        ),
         resolve(),
         commonjs(),
         babel({ babelHelpers: 'bundled' }),
         typescript(),
-        postcss({
-            extract: true, // Extract the CSS to a separate file
-            minimize: true // Minify the CSS
-        }),
         terser()
     ]
 };
-
--- src/components/app-drawer.ts
+++ src/components/app-drawer.ts
@@ -1,106 +1,4 @@
-const css = `
-@font-face {
-  font-family: 'Poppins';
-  src: url('fonts/Poppins/Poppins-Light.ttf') format('truetype');
-  font-weight: 300;
-  font-style: normal;
-}
-
-/* Apply the Poppins font to the component */
-#drawer, .menu-item, .menu-item .text {
-  font-family: 'Poppins', sans-serif;
-}
-
-/* Container for the drawer */
-#drawer {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr); /* 3 columns */
-  gap: 10px; /* Space between grid items */
-  width: 100%;
-  background-color: #fff;
-  padding: 20px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  border-radius: 10px;
-}
-
-/* Individual icon/menu item */
-.menu-item {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 0 5px 5px 5px;
-  background-color: #f9f9f9;
-  border: 2px solid #ccc;
-  border-radius: 10px;
-  cursor: pointer;
-  transition: background-color 0.3s, transform 0.3s, box-shadow 0.3s;
-}
-
-.menu-item.multiorder {
-  border-color: #0691f9;
-  &:hover {
-    border-color: #0691f9;
-    .icon-multiorder { background-image: url('images/blue_ALLinONE MultiOrder.png'); }
-  }
-}
-
-.menu-item.ccs {
-  border-color: #9379ff;
-  &:hover {
-    border-color: #9379ff;
-    .icon-ccs { background-image: url('images/purple_ALLinONE CCS.png'); }
-  }
-}
-
-/* Hover state for menu items */
-.menu-item:hover {
-  background-color: #e0e0e0;
-  transform: translateY(-2px);
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-}
-
-/* Icon within menu item */
-.menu-item .icon {
-  width: 64px; /* Adjust width as needed */
-  height: 64px; /* Adjust height as needed */
-  // margin-bottom: 10px;
-  background-size: contain;
-  background-repeat: no-repeat;
-}
-
-/* Specific icon styles */
-.icon-multiorder { background-image: url('images/Black_ALLinONE MultiOrder.png'); }
-.icon-ccs { background-image: url('images/Black_ALLinONE CCS.png'); }
-.icon-reporting { background-image: url('images/Black_ALLinONE Business Intelligence.png'); }
-.icon-cms { background-image: url('images/Black_cloud hosted.png'); }
-.icon-uc { background-image: url('images/Black_deployment. database.png'); }
-.icon-myaccount { background-image: url('images/Black_ALLinONE MultiOrder.png'); }
-.icon-knowledge-base { background-image: url('images/Black_knowledgeable, brain, information.png'); }
-.icon-ei { background-image: url('images/Black_agility, modular, bespoke.png'); }
-.icon-billing { background-image: url('images/Black_ALLinONE eWarehouse.png'); }
-
-// .icon-multiorder:hover { background-image: url('images/blue_ALLinONE MultiOrder.png'); }
-// .icon-ccs { background-image: url('images/Black_ALLinONE CCS.png'); }
-// .icon-reporting { background-image: url('images/Black_ALLinONE Business Intelligence.png'); }
-// .icon-cms { background-image: url('images/Black_cloud hosted.png'); }
-// .icon-uc { background-image: url('images/Black_deployment. database.png'); }
-// .icon-myaccount { background-image: url('images/Black_ALLinONE MultiOrder.png'); }
-// .icon-knowledge-base { background-image: url('images/Black_knowledgeable, brain, information.png'); }
-// .icon-ei { background-image: url('images/Black_agility, modular, bespoke.png'); }
-// .icon-billing { background-image: url('images/Black_ALLinONE eWarehouse.png'); }
-
-/* Text within menu item */
-.menu-item .text {
-  font-size: 12px;
-  color: #333;
-  text-align: center;
-}
-
-.menu-item .text-knowledge-base {
-    font-size: 9px;
-}
-`;
+import css from "../styles/app-drawer.css" ;
 
 class AppDrawer extends HTMLElement {
     constructor() {
@@ -114,7 +12,8 @@ class AppDrawer extends HTMLElement {
 
     render() {
         const style = document.createElement('style');
-        style.textContent = css;
+        console.log(css)
+        style.textContent = css.toString();
 
         const template = `
           <div id="drawer">
